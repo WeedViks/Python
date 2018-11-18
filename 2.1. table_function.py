e9772710ef5c3178bc40dd0557b6eacaa703d2ec
WeedViks <45105074+WeedViks@users.noreply.github.com>
2018-11-18 23:49:43 +0300

    Add files via upload

---
+++ 2.1. table_function.py
@@ -0,0 +1,33 @@
+def num_to_name(Z):
+    """
+    Определение названия элемента по атомному номеру (Z)
+    """
+
+    if Z==3:
+        return 'Li (литий)'
+    elif Z==17:
+        return 'Cl (хлор)'
+    elif Z==25:
+        return 'Mn (марганец)'
+    elif Z==80:
+        return 'Hg (ртуть)'
+    else:
+        return 'WTF?!'
+    
+Z = int(input('Введи значение Z: '))
+print(num_to_name(Z))
+
+Z = int(input('Введи значение Z: '))
+print(num_to_name(Z))
+
+Z = int(input('Введи значение Z: '))
+print(num_to_name(Z))
+
+Z = int(input('Введи значение Z: '))
+print(num_to_name(Z))
+
+Z = int(input('Введи значение Z: '))
+print(num_to_name(Z))
+
+Z = int(input('Введи значение Z: '))
+print(num_to_name(Z))
