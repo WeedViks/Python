79ac9ae309d590c6f60f5663c3d34233eac09fc3
Victoria <45105074+WeedViks@users.noreply.github.com>
2018-11-20 01:16:21 +0300

    Add files via upload

---
+++ 2.5 cal_tel.py
@@ -0,0 +1,13 @@
+def cal_tel(city,t):
+    if city == 343: return t * 15
+    if city == 381: return t * 18
+    if city == 473: return t * 13
+    if city == 485: return t * 11
+
+    return ('Код города не существует! 0')
+
+
+city = int(input('Введите код города: '))
+t = int(input('Введите время разговора: '))
+
+print('Стоимость разговора составит: ', cal_tel(city,t), 'руб.')
